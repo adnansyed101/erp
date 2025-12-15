14e88f74a76bfc240fca84cc46d6c44374bab85c
adnansyed101 <sirgeant10@gmail.com>
2025-12-16 03:58:29 +0600

    Create homepage.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,7 @@
+import ActivityCard from '@/components/shared/activity-card'
+import type { Links } from '@/lib/types/general.types'
+import MainLayout from '@/providers/main-layout'
+import { createFileRoute } from '@tanstack/react-router'
 import {
   Boxes,
   Users,
@@ -20,104 +24,105 @@ import {
   Home,
   Settings,
 } from 'lucide-react'
-import MainLayout from '../provider/MainLayout'
-import { Links } from '@/lib/types/types'
-import ActivityCard from '@/components/cards/activity-card'
 
-const activities = [
-  {
-    icon: Boxes,
-    title: 'Supply Chain Management System',
-    link: '/supply-chain',
-  },
-  {
-    icon: Users,
-    title: 'Customer Relationship Management',
-    link: '/customer-relationship',
-  },
-  {
-    icon: Briefcase,
-    title: 'Human Resource Management System',
-    link: '/hr-management',
-  },
-  {
-    icon: DollarSign,
-    title: 'Accounting Management System',
-    link: '/accounting-management',
-  },
-  {
-    icon: TrendingUp,
-    title: 'Payroll Management System',
-    link: '/payroll-management',
-  },
-  {
-    icon: MapPin,
-    title: 'Field Force',
-    link: '/field-force',
-  },
-  {
-    icon: CheckSquare,
-    title: 'Task Management System',
-    link: '/task-management',
-  },
-  {
-    icon: ShoppingCart,
-    title: 'Sales Management System',
-    link: '/sales-management',
-  },
-  {
-    icon: GitBranch,
-    title: 'Workflow Management System',
-    link: '/workflow-management',
-  },
-  {
-    icon: Zap,
-    title: 'Recruitment Management System',
-    link: '/recruitment-management',
-  },
-  {
-    icon: Package,
-    title: 'Inventory Management System',
-    link: '/invenentory-managemnt',
-  },
-  {
-    icon: Building2,
-    title: 'Fixed Asset Management System',
-    link: '/fixed-asset',
-  },
-  {
-    icon: Heart,
-    title: 'Home Care',
-    link: '/home-care',
-  },
-  {
-    icon: CreditCard,
-    title: 'Point of Sale',
-    link: '/point-of-sale',
-  },
-  {
-    icon: Banknote,
-    title: 'Non Banking',
-    link: '/non-banking',
-  },
-  {
-    icon: Truck,
-    title: 'Rental',
-    link: '/rental',
-  },
-  {
-    icon: Shield,
-    title: 'Dispatch Safety and Security',
-    link: '/dispatch-safety-and-security',
-  },
-  {
-    icon: HandshakeIcon,
-    title: 'Partner',
-    link: '/partner',
-  },
-]
+export const Route = createFileRoute('/home')({
+  component: RouteComponent,
+})
 
-export default function HomePage() {
+function RouteComponent() {
+  const activities = [
+    {
+      icon: Boxes,
+      title: 'Supply Chain Management System',
+      link: '/supply-chain',
+    },
+    {
+      icon: Users,
+      title: 'Customer Relationship Management',
+      link: '/customer-relationship',
+    },
+    {
+      icon: Briefcase,
+      title: 'Human Resource Management System',
+      link: '/hr-management',
+    },
+    {
+      icon: DollarSign,
+      title: 'Accounting Management System',
+      link: '/accounting-management',
+    },
+    {
+      icon: TrendingUp,
+      title: 'Payroll Management System',
+      link: '/payroll-management',
+    },
+    {
+      icon: MapPin,
+      title: 'Field Force',
+      link: '/field-force',
+    },
+    {
+      icon: CheckSquare,
+      title: 'Task Management System',
+      link: '/task-management',
+    },
+    {
+      icon: ShoppingCart,
+      title: 'Sales Management System',
+      link: '/sales-management',
+    },
+    {
+      icon: GitBranch,
+      title: 'Workflow Management System',
+      link: '/workflow-management',
+    },
+    {
+      icon: Zap,
+      title: 'Recruitment Management System',
+      link: '/recruitment-management',
+    },
+    {
+      icon: Package,
+      title: 'Inventory Management System',
+      link: '/invenentory-managemnt',
+    },
+    {
+      icon: Building2,
+      title: 'Fixed Asset Management System',
+      link: '/fixed-asset',
+    },
+    {
+      icon: Heart,
+      title: 'Home Care',
+      link: '/home-care',
+    },
+    {
+      icon: CreditCard,
+      title: 'Point of Sale',
+      link: '/point-of-sale',
+    },
+    {
+      icon: Banknote,
+      title: 'Non Banking',
+      link: '/non-banking',
+    },
+    {
+      icon: Truck,
+      title: 'Rental',
+      link: '/rental',
+    },
+    {
+      icon: Shield,
+      title: 'Dispatch Safety and Security',
+      link: '/dispatch-safety-and-security',
+    },
+    {
+      icon: HandshakeIcon,
+      title: 'Partner',
+      link: '/partner',
+    },
+  ]
+
   const links: Links = [
     {
       label: 'Essential Links',
