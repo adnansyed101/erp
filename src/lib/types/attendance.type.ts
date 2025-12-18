1c7947403c0a5fc2eeed57012bc09769c95c3218
adnansyed101 <sirgeant10@gmail.com>
2025-12-18 11:32:39 +0600

    Update import.

--- src/lib/types/attendance.type.ts
+++ src/lib/types/attendance.type.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 import {
   AttendanceSchema,
   AttendanceSchemaWithEmployeeData,
-} from '../validators/attendance.schema'
+} from '../validators/attendance.validator'
 
 export type Attendance = z.infer<typeof AttendanceSchema>
 export type AttendanceWithEmployeeData = z.infer<
