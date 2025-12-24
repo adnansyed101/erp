694a23f9d28a597d6da1f78a19d7f9040e469a35
adnansyed101 <sirgeant10@gmail.com>
2025-12-24 17:02:39 +0600

    Add grid cols.

--- src/routes/hr-management/employee-list.tsx
+++ src/routes/hr-management/employee-list.tsx
@@ -38,7 +38,7 @@ function EmployeeListPage() {
         {isLoading || isPending ? (
           'Loading....'
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-6">
             {employees.data.map((employee: EmployeeWithId) => (
               <EmployeeCard
                 key={employee.id}
