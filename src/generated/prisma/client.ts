d2e7019891c5c3ce372c87ff9feb45103d8e12d2
adnansyed101 <sirgeant10@gmail.com>
2025-12-30 19:29:04 +0600

    Generate prisma.

--- src/generated/prisma/client.ts
+++ src/generated/prisma/client.ts
@@ -74,3 +74,23 @@ export type Attendance = Prisma.AttendanceModel
  * 
  */
 export type Employee = Prisma.EmployeeModel
+/**
+ * Model User
+ * 
+ */
+export type User = Prisma.UserModel
+/**
+ * Model Session
+ * 
+ */
+export type Session = Prisma.SessionModel
+/**
+ * Model Account
+ * 
+ */
+export type Account = Prisma.AccountModel
+/**
+ * Model Verification
+ * 
+ */
+export type Verification = Prisma.VerificationModel
