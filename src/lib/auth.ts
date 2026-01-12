fde67ea1a2913e5ec2f354a6dbafcf1c0fb16352
adnansyed101 <sirgeant10@gmail.com>
2026-01-12 14:30:23 +0600

    Create auth files using better auth docs.

---
+++ src/lib/auth.ts
@@ -0,0 +1,52 @@
+import { betterAuth } from 'better-auth'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+const prisma = new PrismaClient({ adapter })
+
+export const auth = betterAuth({
+  baseURL: import.meta.env.VITE_APP_URL,
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 10 * 60,
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: [
+          'CEO',
+          'CTO',
+          'ENGINEERING_MANAGER',
+          'TECH_LEAD',
+          'DEVELOPER',
+          'DESIGNER',
+          'PROJECT_MANAGER',
+          'HR',
+        ],
+        required: true,
+        defaultValue: 'employee',
+        input: true,
+      },
+    },
+  },
+  plugins: [tanstackStartCookies()],
+})
+
+export type Session = typeof auth.$Infer.Session
+export type User = typeof auth.$Infer.Session.user
