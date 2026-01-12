08e44dbad47711af4b84f0a9e7a1c99f5a12d3a0
adnansyed101 <sirgeant10@gmail.com>
2026-01-12 14:41:58 +0600

    Create middlewares for authorization.

---
+++ src/middleware/auth.ts
@@ -0,0 +1,43 @@
+import { redirect } from '@tanstack/react-router'
+import { createMiddleware } from '@tanstack/react-start'
+import { getRequestHeaders } from '@tanstack/react-start/server'
+import { auth } from '@/lib/auth'
+
+export const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const headers = getRequestHeaders()
+  const session = await auth.api.getSession({ headers })
+
+  if (!session) {
+    throw redirect({ to: '/' })
+  }
+
+  return await next()
+})
+
+export const authLoginMiddleware = createMiddleware().server(
+  async ({ next }) => {
+    const headers = getRequestHeaders()
+    const session = await auth.api.getSession({ headers })
+
+    if (session) {
+      throw redirect({ to: '/home' })
+    }
+
+    return await next()
+  },
+)
+
+export const authApiMiddleware = createMiddleware().server(async ({ next }) => {
+  const headers = getRequestHeaders()
+  const session = await auth.api.getSession({ headers })
+
+  if (!session) {
+    return Response.json({
+      success: false,
+      data: null,
+      message: 'You are not authorized to use this API.',
+    })
+  }
+
+  return await next()
+})
