14407d109ac29dcc7ddabf03870a79ce74ba3fb9
adnansyed101 <sirgeant10@gmail.com>
2026-01-19 16:57:38 +0600

    Show user details.

--- src/components/sidebar/nav-user.tsx
+++ src/components/sidebar/nav-user.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   BadgeCheck,
   Bell,
@@ -32,19 +30,19 @@ import { useTransition } from 'react'
 import { Spinner } from '../ui/spinner'
 import { toast } from 'sonner'
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+export function NavUser() {
   const { isMobile } = useSidebar()
   const [isTransitionPending, startTransition] = useTransition()
-
+  const { data: session, isPending, error } = authClient.useSession()
   const navigate = useNavigate()
+
+  if (isPending) {
+    return <Spinner />
+  }
+
+  if (error) {
+    return 'Some Error occured in rendering sidebar.'
+  }
 
   return (
     <SidebarMenu>
@@ -56,14 +54,19 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage
+                  src={session?.user.image ?? ''}
+                  alt={session?.user.name}
+                />
                 <AvatarFallback className="rounded-lg">
                   <User />
                 </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">
+                  {session?.user.name}
+                </span>
+                <span className="truncate text-xs">{session?.user.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -77,12 +80,19 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage
+                    src={session?.user.image ?? ''}
+                    alt={session?.user.name}
+                  />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">
+                    {session?.user.name}
+                  </span>
+                  <span className="truncate text-xs">
+                    {session?.user.email}
+                  </span>
                 </div>
               </div>
             </DropdownMenuLabel>
