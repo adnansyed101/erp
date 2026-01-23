3c8975fa33d4a77a3228b71af210ea83847cfb7c
adnansyed101 <sirgeant10@gmail.com>
2026-01-23 16:07:22 +0600

    Update type name.

--- src/lib/types/leave.types.ts
+++ src/lib/types/leave.types.ts
@@ -1,4 +1,4 @@
 import z from 'zod'
 import { applyLeaveSchema } from '../validators/leave.validator'
 
-export type ApplyLeaveFormValues = z.infer<typeof applyLeaveSchema>
+export type ApplyLeave = z.infer<typeof applyLeaveSchema>
