8054488ef2ebe24edcaf67628895d5d33402e928
adnansyed101 <sirgeant10@gmail.com>
2026-01-23 16:04:26 +0600

    Get and Create new leave.

---
+++ src/routes/api/hr-management/leave-management.ts
@@ -0,0 +1,49 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { prisma } from '@/db'
+import { formatError } from '@/lib/utils'
+import { ApplyLeave } from '@/lib/types/leave.types'
+
+export const Route = createFileRoute('/api/hr-management/leave-management')({
+  server: {
+    handlers: {
+      GET: async () => {
+        try {
+          const leaves = await prisma.leaveManagement.findMany()
+          return Response.json({
+            success: true,
+            data: leaves,
+            message: 'Fetched all leaves.',
+          })
+        } catch (error) {
+          return Response.json({
+            success: false,
+            data: [],
+            message: formatError(error),
+          })
+        }
+      },
+      POST: async ({ request }) => {
+        const body: ApplyLeave = await request.json()
+
+        try {
+          const newLeave = await prisma.leaveManagement.create({
+            data: body,
+          })
+
+          return Response.json({
+            success: true,
+            data: newLeave,
+            message: 'Leave applied successfully.',
+          })
+        } catch (error) {
+          console.log(error)
+          return Response.json({
+            success: false,
+            data: {},
+            message: formatError(error),
+          })
+        }
+      },
+    },
+  },
+})
