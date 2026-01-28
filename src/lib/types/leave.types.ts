7f0482a88eca46af67cd230434e4e99b57b96d9d
adnansyed101 <sirgeant10@gmail.com>
2026-01-28 13:59:48 +0600

    Create type for updating leave.

--- src/lib/types/leave.types.ts
+++ src/lib/types/leave.types.ts
@@ -2,7 +2,9 @@ import z from 'zod'
 import {
   applyLeaveSchema,
   applyLeaveSchemaWithId,
+  updateLeaveSchema,
 } from '../validators/leave.validator'
 
 export type ApplyLeave = z.infer<typeof applyLeaveSchema>
 export type ApplyLeaveWithId = z.infer<typeof applyLeaveSchemaWithId>
+export type UpdateLeaveSchema = z.infer<typeof updateLeaveSchema>
